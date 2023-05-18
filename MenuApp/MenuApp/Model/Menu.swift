25bffbf6cd09b0e5907abae3cec6e5312898c73d
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-18 17:24:06 +0900

    [feat] #1 음식 정보를 가지고 있는 `Menu` 타입 구현
    - 음식 이름과 음식의 카테고리를 가지고 있는 `Menu` 타입 구현
    - 내부에 `FoodCategory` 열거형으로 음식 종류에 따라 `category` 속성에 할당
    - View를 구현하며 `FoodCategory`를 별도로 사용하는 경우가 발생하는 경우 타입 분리 예정
    - 이미지의 경우 아직 필요한 사항이 아니므로 추후 이미지를 불러오는 방식에 따라 Menu 타입에 넣을 예정
    
    Closes #1

---
+++ MenuApp/MenuApp/Model/Menu.swift
@@ -0,0 +1,36 @@
+//
+//  Menu.swift
+//  MenuApp
+//
+//  Created by Blu on 2023/05/18.
+//
+
+import Foundation
+
+struct Menu {
+    var name: String
+    var category: FoodCategory
+
+    enum FoodCategory: CustomStringConvertible {
+        case korean
+        case chinese
+        case japanese
+        case western
+        case fusion
+
+        var description: String {
+            switch self {
+            case .korean:
+                return "한식"
+            case .chinese:
+                return "중식"
+            case .japanese:
+                return "일식"
+            case .western:
+                return "양식"
+            case .fusion:
+                return "퓨전"
+            }
+        }
+    }
+}
