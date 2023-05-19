919885e1879fedf080ecc3b371758a0629e5d83f
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-20 03:49:10 +0900

    [feat] #2 탭 바 아이템의 종류를 가지고 있는 `ViewCategory` 타입 구현
    - `list`, `calendar`, `pushAlert`을 케이스로 가지고 있는 열거형 타입 구현
    - 각 케이스별 뷰의 타이틀과 시스템 이미지 이름을 가지는 속성 추가

--- MenuApp/MenuApp/Model/ViewCategory.swift
+++ MenuApp/MenuApp/Model/ViewCategory.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum ViewCategory: Int {
+    case list
+    case calendar
+    case pushAlert
+
+    var title: String {
+        switch self {
+        case .list:
+            return "리스트"
+        case .calendar:
+            return "캘린더"
+        case .pushAlert:
+            return "푸시알림"
+        }
+    }
+
+    var imageName: String {
+        switch self {
+        case .list:
+            return "list.bullet.clipboard"
+        case .calendar:
+            return "calendar"
+        case .pushAlert:
+            return "bell"
+        }
+    }
+}
