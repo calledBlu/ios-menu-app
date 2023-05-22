dc9fd66f661d8c11d2269cf62e3ef87c21455930
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-22 13:35:07 +0900

    [feat] #3 `DateProvider` 타입 구현
    - Model과 Controller 사이에서 데이터 전달 및 업데이트를 담당할 `DateProvider` 타입 구현
    - `configureCalendar()`: 캘린더의 일자 및 일자 수를 계산할 기본 구현 메서드
    - `updateDate(_: )`: `calendarDate` 변경 시 사용할 메서드
    - `updateCalendarTitle()`: 캘린더의 년/월에 해당하는 레이블에 들어갈 String 값을 업데이트하는 메서드
    - `calculateStartWeekday() -> Int`: 해당 월의 시작 요일을 계산, days 배열의 0번 index를 일요일로 해주기 위해 -1 을 계산해주는 메서드
    - `calculateEndDate() -> Int`: 한 달이 총 며칠로 구성되어 있는지를 계산하는 메서드
    - `updateDays()`: 캘린더에 일자로 뿌려질 days를 구성하는 메서드, 계산 과정에서 위의 시작 요일 계산, 한 달이 며칠로 구성되어 있는지 계산하는 메서드가 활용됨

---
+++ MenuApp/MenuApp/Model/DateProvider.swift
@@ -0,0 +1,59 @@
+//
+//  DateProvider.swift
+//  MenuApp
+//
+//  Created by Blu on 2023/05/22.
+//
+
+import Foundation
+
+final class DateProvider {
+    var menuCalendar = MenuCalendar()
+
+    func configureCalendar() {
+        let components = menuCalendar.calendar.dateComponents([.year, .month], from: Date())
+        guard let newDate = menuCalendar.calendar.date(from: components) else {
+            return
+        }
+
+        menuCalendar.calendarDate = newDate
+        updateDays()
+    }
+
+    func updateDate(_ date: Date) {
+        menuCalendar.calendarDate = date
+    }
+
+    func updateCalendarTitle() -> String {
+        menuCalendar.dateFormatter.dateFormat = "YYYY년 MM월"
+        return menuCalendar.dateFormatter.string(from: menuCalendar.calendarDate)
+    }
+
+    private func calculateStartWeekday() -> Int {
+        return menuCalendar.calendar.component(.weekday, from: menuCalendar.calendarDate) - 1
+    }
+
+    private func calculateEndDate() -> Int {
+        guard let endDate = menuCalendar.calendar.range(of: .day,
+                                                        in: .month,
+                                                        for: menuCalendar.calendarDate)?.count else {
+            return 0
+        }
+
+        return endDate
+    }
+
+    private func updateDays() {
+        menuCalendar.days.removeAll()
+        let startWeekDay = calculateStartWeekday()
+        let totalDays = startWeekDay + calculateEndDate()
+
+        for day in 0..<totalDays {
+            if day < startWeekDay {
+                menuCalendar.days.append("")
+                continue
+            }
+            menuCalendar.days.append("\(day - startWeekDay + 1)")
+        }
+    }
+}
