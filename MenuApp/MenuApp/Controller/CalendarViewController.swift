5ee736654ebe34bfdf74d94daafdeee98d1b0709
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-25 14:08:38 +0900

    [feat] #3 `CalendarView` 구현
    - `dateProvider`: `DateProvider` 타입을 통해 월에 해당하는 데이터를 받아오기 위한 프로퍼티
    - `defaultIndexPath`: 선택됐을 때의 날짜에 해당하는 IndexPath 값을 가지고 있는 프로퍼티, 초기에 선택된 날짜를 deselect 상태로 되돌리기 위해 사용
    - `calendarTitle`의 대리자를 `CalendarViewController`로 지정하여 이전달, 다음달로 이동하는 버튼을 누르는 경우 보여지는 달이 바뀌도록 구현
    
    Closes #3

--- MenuApp/MenuApp/Controller/CalendarViewController.swift
+++ MenuApp/MenuApp/Controller/CalendarViewController.swift
@@ -8,62 +8,198 @@
 import UIKit
 
 final class CalendarViewController: UIViewController {
+
     // MARK: - Properties
+
     private var dateProvider = DateProvider()
+    private var defaultIndexPath: IndexPath?
 
     // MARK: - UI Components
-    private lazy var headerView = UIView()
+
+    private lazy var scrollView = UIScrollView()
+    private lazy var contentView = UIView()
     private lazy var calendarTitle = CalendarTitleStackView(frame: .zero)
     private lazy var weekStackView = WeekStackView()
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
 
     // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setup()
+        self.view.backgroundColor = .systemBackground
+
+        configure()
+        updateCalendar()
     }
 
-    // MARK: - Public Methods
-
     // MARK: - Private Methods
+
+    private func updateCalendar() {
+        let metaData = try? dateProvider.generateMonthMetaData(for: dateProvider.sendCurrentCalendarDate())
+        let newDays = try? dateProvider.generateDaysInMonth(for: dateProvider.sendCurrentCalendarDate())
+
+        dateProvider.configureMonthMetaData(metaData)
+        dateProvider.updateDaysInMonth(newDays)
+
+        updateTitle()
+    }
+
+    private func configurationDelegateAndFunction() {
+        scrollView.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        calendarTitle.delegate = self
+    }
+
+    private func updateTitle() {
+        let date = dateProvider.titleDateFormatter.string(from: dateProvider.sendCurrentCalendarDate())
+        calendarTitle.updateDateTitle(date: date)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension CalendarViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(cell: CalendarCollectionViewCell.self, for: indexPath)
+        let daysInMonth = dateProvider.sendDaysInMonth()
+
+        cell.updateDay(day: daysInMonth[indexPath.item])
+
+        if daysInMonth[indexPath.item].isSelected == true {
+            cell.changeIsSelectStatus()
+            defaultIndexPath = indexPath
+        }
+        dateProvider.updateDaysInMonth(with: indexPath, value: false)
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let defaultCell = collectionView.cellForItem(at: defaultIndexPath!) as? CalendarCollectionViewCell {
+            defaultCell.changeIsDeselectStatus()
+        }
+
+        if let cell = collectionView.cellForItem(at: indexPath) as? CalendarCollectionViewCell {
+            cell.changeIsSelectStatus()
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) as? CalendarCollectionViewCell {
+            cell.changeIsDeselectStatus()
+        }
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension CalendarViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.weekStackView.frame.width / 7
+        let height = collectionView.frame.height
+        let collectionViewHeight = dateProvider.sendDaysInMonth().count == 35 ? (height / 5) : (height / 5.5)
+
+        return CGSize(width: width, height: collectionViewHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+}
+
+// MARK: - CalendarTitleStackViewDelegate
+
+extension CalendarViewController: CalendarTitleStackViewDelegate {
+    func movePreviousMonth() {
+        let previous = dateProvider.calendar.date(byAdding: DateComponents(month: -1),
+                                                  to: dateProvider.sendCurrentCalendarDate())
+        dateProvider.updateCalendarDate(previous)
+        updateCalendar()
+        collectionView.reloadData()
+    }
+
+    func moveNextMonth() {
+        let next = dateProvider.calendar.date(byAdding: DateComponents(month: 1),
+                                              to: dateProvider.sendCurrentCalendarDate())
+        dateProvider.updateCalendarDate(next)
+        updateCalendar()
+        collectionView.reloadData()
+    }
+}
+
+extension CalendarViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dateProvider.sendDaysInMonth().count
+    }
 }
 
 // MARK: - UI & Layout
+
 extension CalendarViewController {
-    private func setup() {
-        setUI()
-        setupLayout()
-    }
-
-    private func setUI() {
+    private func configure() {
         configureNavigationBar()
-        calendarTitle.updateDateTitle(date: dateProvider.updateCalendarTitle())
-
-        view.addSubview(headerView)
-        view.addSubview(weekStackView)
-
-        headerView.addSubview(calendarTitle)
-
+        configureUI()
+        configureLayout()
+        configurationDelegateAndFunction()
     }
 
-    private func setupLayout() {
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+    private func configureUI() {
+        view.addSubview(contentView)
+        contentView.addSubview(calendarTitle)
+        contentView.addSubview(weekStackView)
+        contentView.addSubview(scrollView)
+        scrollView.addSubview(collectionView)
+
+        collectionView.register(cell: CalendarCollectionViewCell.self)
+    }
+
+    private func configureLayout() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsVerticalScrollIndicator = false
 
         NSLayoutConstraint.activate([
-            headerView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 70),
+            contentView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 49),
 
             calendarTitle.heightAnchor.constraint(equalToConstant: 40),
             calendarTitle.widthAnchor.constraint(equalToConstant: view.frame.size.width / 1.7),
-            calendarTitle.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
-            calendarTitle.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+            calendarTitle.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            calendarTitle.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
 
-            weekStackView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
-            weekStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            weekStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            weekStackView.heightAnchor.constraint(equalToConstant: 40)
+            weekStackView.topAnchor.constraint(equalTo: calendarTitle.bottomAnchor, constant: 20),
+            weekStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            weekStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            weekStackView.heightAnchor.constraint(equalToConstant: 40),
+
+            scrollView.topAnchor.constraint(equalTo: weekStackView.bottomAnchor, constant: 10),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.readableContentGuide.bottomAnchor),
+
+            collectionView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            collectionView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+
+            collectionView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            collectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7)
         ])
     }
 
