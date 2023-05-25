1439e8651f5e688bb58919ef6456c580f1b84661
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-25 13:47:07 +0900

    [feat] #3 `DateProvider` 타입 구현
    - 데이터의 생성, 전달, 업데이트를 전담하는 타입 구현
    - 모델의 데이터를 컨트롤러에 전달하는 역할을 함
    - 모델의 데이터(예: `MenuCalendar`, `MonthMetadata`)에 컨트롤러가 직접 접근할 수 없도록 `private` 키워드를 붙여주고, 이를 전달하는 메서드 구현
    - `DateProvidable` 프로토콜을 채택하여 해당 프로토콜의 extension으로 기본 구현된 메서드를 사용

---
+++ MenuApp/MenuApp/Model/Provider/DateProvider.swift
@@ -0,0 +1,79 @@
+//
+//  DateProvider.swift
+//  MenuApp
+//
+//  Created by Blu on 2023/05/22.
+//
+
+import Foundation
+
+final class DateProvider: DateProvidable {
+    
+    // MARK: - Properties
+
+    private var menuCalendar = MenuCalendar()
+    private var monthMetaData: MonthMetadata?
+
+    let calendar: Calendar
+    let titleDateFormatter: DateFormatter
+    let dayDateFormatter: DateFormatter
+    let selectedDate = Date()
+
+    // MARK: - Initialization
+
+    init() {
+        self.calendar = Calendar(identifier: .gregorian)
+        self.titleDateFormatter = DateFormatter()
+        self.dayDateFormatter = DateFormatter()
+
+        self.titleDateFormatter.dateFormat = CalendarDateFormat.yearAndMonth.format
+        self.dayDateFormatter.dateFormat = CalendarDateFormat.day.format
+    }
+
+    // MARK: - Public Methods
+
+    func configureMonthMetaData(_ data: MonthMetadata?) {
+        monthMetaData = data
+    }
+
+    func updateDaysInMonth(_ newDays: [Day]?) {
+        guard let newDays = newDays else {
+            return
+        }
+
+        menuCalendar.daysInMonth = newDays
+    }
+
+    func sendCurrentCalendarDate() -> Date {
+        return menuCalendar.calendarDate
+    }
+
+    func updateCalendarDate(_ date: Date?) {
+        guard let date = date else {
+            return
+        }
+
+        menuCalendar.calendarDate = date
+    }
+
+    func sendDaysInMonth() -> [Day] {
+        guard let daysInMonth = menuCalendar.daysInMonth else {
+            return []
+        }
+        return daysInMonth
+    }
+
+    func updateDaysInMonth(with index: IndexPath, value: Bool) {
+        menuCalendar.daysInMonth?[index.item].isSelected = value
+    }
+
+    // MARK: - Private Methods
+
+    private func startDayOfTheWeek() -> Int {
+        return calendar.component(.weekday, from: menuCalendar.calendarDate) - 1
+    }
+
+    private func endDate() -> Int {
+        return calendar.range(of: .day, in: .month, for: menuCalendar.calendarDate)?.count ?? Int()
+    }
+}
