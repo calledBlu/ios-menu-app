0d8b3593dd47281688cc807ddbc7150211a0118f
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-25 13:17:48 +0900

    [feat] #3 `MonthMetadata` 타입 구현
    -  한 달의 데이터를 구조화하여 담고있는 타입인 `MonthMetadata` 타입 구현
    - `numberOfDays`: 해당 월에 총 며칠이 있는지에 대한 정보를 가지고 있는 프로퍼티
    - `firstDay`: 해당 월의 첫 날짜를 `Date` 타입으로 가지고 있는 프로퍼티
    - `firstWeekday`: 해당 월의 첫 날이 어떤 요일에 해당하는지에 대한 정보를 `Int`타입으로 가지고 있는 프로퍼티, 일요일 = 1을 기준으로 함

---
+++ MenuApp/MenuApp/Model/Calendar/MonthMetadata.swift
@@ -0,0 +1,14 @@
+//
+//  MonthMetadata.swift
+//  MenuApp
+//
+//  Created by Blu on 2023/05/25.
+//
+
+import Foundation
+
+struct MonthMetadata {
+    let numberOfDays: Int
+    let firstDay: Date
+    let firstWeekday: Int
+}
