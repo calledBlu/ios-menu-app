6f5665742800593473502a79262281d6924f9c66
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-25 13:44:33 +0900

    [feat] #3 날짜를 전달, 업데이트할 타입의 기본 기능을 가지고 있는 `DateProvidable` 프로토콜 구현
    - 해당 프로토콜을 채택하는 경우 날짜를 전달하는 데 필요한 기본 캘린더, 데이터 포맷터, 선택된 날짜를 가지고 있어야 함을 명시
    - 날짜를 전달, 업데이트 하기에 앞서 날짜를 기본 구성하는 메서드를 명시하고 extension으로 초기 구현

---
+++ MenuApp/MenuApp/Model/Provider/Interface/DateProvidable.swift
@@ -0,0 +1,87 @@
+//
+//  DateProvidable.swift
+//  MenuApp
+//
+//  Created by Blu on 2023/05/25.
+//
+
+import Foundation
+
+protocol DateProvidable {
+    var calendar: Calendar { get }
+    var titleDateFormatter: DateFormatter { get }
+    var dayDateFormatter: DateFormatter { get }
+    var selectedDate: Date { get }
+
+    func generateMonthMetaData(for baseDate: Date) throws -> MonthMetadata
+    func generateDaysInMonth(for baseDate: Date) throws -> [Day]
+    func generateStartOfNextMonth(for firstDayOfDisplayedMonth: Date) -> [Day]
+    func generateDay(offsetBy dayOffset: Int, for baseDate: Date, isWithinDisplayedMonth: Bool) -> Day
+}
+
+extension DateProvidable {
+    func generateMonthMetaData(for baseDate: Date) throws -> MonthMetadata {
+        guard let numberOfDaysInMonth = calendar.range(of: .day, in: .month, for: baseDate)?.count,
+              let firstDayOfMonth = calendar.date(from: calendar.dateComponents([.year, .month],
+                                                                                from: baseDate)) else {
+            throw CalendarDataError.doNotGenerateMetaData
+        }
+
+        let firstDayWeekday = calendar.component(.weekday, from: firstDayOfMonth)
+
+        return MonthMetadata(numberOfDays: numberOfDaysInMonth,
+                             firstDay: firstDayOfMonth,
+                             firstWeekday: firstDayWeekday)
+    }
+
+    func generateDaysInMonth(for baseDate: Date) throws -> [Day] {
+        guard let monthMetaData = try? generateMonthMetaData(for: baseDate) else {
+            throw CalendarDataError.doNotGenerateMetaData
+        }
+
+        let numberOfDaysInMonth = monthMetaData.numberOfDays
+        let offsetInInitialRow = monthMetaData.firstWeekday
+        let firstDayOfMonth = monthMetaData.firstDay
+
+        var days: [Day] = (1..<(numberOfDaysInMonth + offsetInInitialRow)).map { day in
+            let isWithinDisplayedMonth = day >= offsetInInitialRow
+            let dayOffset = isWithinDisplayedMonth ? day - offsetInInitialRow : -(offsetInInitialRow - day)
+
+            return generateDay(offsetBy: dayOffset,
+                               for: firstDayOfMonth,
+                               isWithinDisplayedMonth: isWithinDisplayedMonth)
+        }
+
+        days += generateStartOfNextMonth(for: firstDayOfMonth)
+
+        return days
+    }
+
+    func generateStartOfNextMonth(for firstDayOfDisplayedMonth: Date) -> [Day] {
+        guard let lastDayInMonth = calendar.date(byAdding: DateComponents(month: 1, day: -1),
+                                                 to: firstDayOfDisplayedMonth) else {
+            return []
+        }
+
+        let additionalDays = 7 - calendar.component(.weekday, from: lastDayInMonth)
+
+        if additionalDays == 0 {
+            return []
+        }
+
+        let days: [Day] = (1...additionalDays).map {
+            generateDay(offsetBy: $0, for: lastDayInMonth, isWithinDisplayedMonth: false)
+        }
+
+        return days
+    }
+
+    func generateDay(offsetBy dayOffset: Int, for baseDate: Date, isWithinDisplayedMonth: Bool) -> Day {
+        let date = calendar.date(byAdding: .day, value: dayOffset, to: baseDate) ?? baseDate
+
+        return Day(date: date,
+                   number: dayDateFormatter.string(from: date),
+                   isSelected: calendar.isDate(date, inSameDayAs: selectedDate),
+                   isWithinDisplayedMonth: isWithinDisplayedMonth)
+    }
+}
