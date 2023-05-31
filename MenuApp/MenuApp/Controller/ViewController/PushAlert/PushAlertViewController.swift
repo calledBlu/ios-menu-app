d6040ce41b7a5781724790f7d7a3a3b3405c63aa
calledBlu <71758542+calledBlu@users.noreply.github.com>
2023-05-31 19:35:35 +0900

    [feat] Push Alert을 활성화/비활성화 시키는 화면 및 기능 구현

--- MenuApp/MenuApp/Controller/ViewController/PushAlert/PushAlertViewController.swift
+++ MenuApp/MenuApp/Controller/ViewController/PushAlert/PushAlertViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 final class PushAlertViewController: UIViewController {
 
+    // MARK: - Properties
+    private let alertKind = AlertKind.allCases
+
+    // MARK: - UI Components
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationBar()
+        setup()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(cell: PushAlertCell.self)
     }
 
     // MARK: - Private Methods
@@ -25,5 +35,69 @@ final class PushAlertViewController: UIViewController {
 
         let leftBarButton = UIBarButtonItem(customView: titleLabel)
         navigationItem.leftBarButtonItems = [leftBarButton]
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension PushAlertViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return alertKind.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(cell: PushAlertCell.self, for: indexPath)
+        cell.updateAlertKind(alertKind[indexPath.item])
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension PushAlertViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = 40.0
+
+        return CGSize(width: width, height: height)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+}
+
+// MARK: - UI & Layout
+extension PushAlertViewController {
+    private func setup() {
+        setUI()
+        setupLayout()
+    }
+
+    private func setUI() {
+        view.addSubview(collectionView)
+    }
+
+    private func setupLayout() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor, constant: 24),
+            collectionView.bottomAnchor.constraint(equalTo: view.readableContentGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
--- MenuApp/MenuApp/View/PushAlert/PushAlertCell.swift
+++ MenuApp/MenuApp/View/PushAlert/PushAlertCell.swift
@@ -5,4 +5,71 @@
 //  Created by Blu on 2023/05/30.
 //
 
-import Foundation
+import UIKit
+
+final class PushAlertCell: UICollectionViewCell {
+
+    private let userNotificationCenter = UNUserNotificationCenter.current()
+    private var alert: AlertKind?
+
+    private lazy var alertTitleLabel = UILabel()
+    private lazy var alertSwitch = UISwitch()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+        alertSwitch.addTarget(self, action: #selector(alertSwitchToggle), for: .valueChanged)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func updateAlertKind(_ kind: AlertKind) {
+        self.alert = kind
+        alertSwitch.isOn = UserDefaults.standard.bool(forKey: kind.description)
+        configure()
+    }
+
+    private func configure() {
+        configureUI()
+        configureLayout()
+    }
+
+    private func configureUI() {
+        self.addSubview(alertTitleLabel)
+        self.addSubview(alertSwitch)
+        alertTitleLabel.text = alert?.description
+        alertTitleLabel.font = UIFont(name: "Pretendard-Regular", size: 16)
+        alertSwitch.preferredStyle = .sliding
+    }
+
+    private func configureLayout() {
+        alertTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        alertSwitch.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            alertTitleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            alertTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+
+            alertSwitch.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            alertSwitch.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5)
+        ])
+    }
+
+    @objc func alertSwitchToggle(sender: UISwitch) {
+        guard let data = alert?.data,
+              let key = alert?.description else {
+            return
+        }
+
+        if sender.isOn == true {
+            UNUserNotificationCenter.current().setNotifiation(data)
+        } else {
+            UNUserNotificationCenter.current().removeNotification(data)
+        }
+
+        UserDefaults.standard.set(sender.isOn, forKey: key)
+    }
+}
